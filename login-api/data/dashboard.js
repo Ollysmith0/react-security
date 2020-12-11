95684ad476e312b88c23a09d424e568cf300ccef
Tu <tuhuynh@r2integrated.com>
2020-12-11 14:33:53 +0700

    ewo

---
+++ login-api/data/dashboard.js
@@ -0,0 +1,19 @@
+module.exports = {
+  salesVolume: 100000,
+  newCustomers: 100,
+  refunds: 2000,
+  graphData: [
+    { date: "Jan 2020", amount: 70 },
+    { date: "Feb 2020", amount: 123 },
+    { date: "Mar 2020", amount: 234 },
+    { date: "Apr 2020", amount: 378 },
+    { date: "May 2020", amount: 575 },
+    { date: "Jun 2020", amount: 642 },
+    { date: "Jul 2020", amount: 511 },
+    { date: "Aug 2020", amount: 798 },
+    { date: "Sep 2020", amount: 497 },
+    { date: "Oct 2020", amount: 490 },
+    { date: "Nov 2020", amount: 598 },
+    { date: "Dec 2020", amount: 899 },
+  ],
+};
